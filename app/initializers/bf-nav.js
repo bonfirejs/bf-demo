a94ed925cdd76b5a2dd1c20f4667a45b410a04ad
Nathan Ward <ward@digivine.com>
2015-03-29 11:54:36 -0700

    Move nav component to addon

--- app/initializers/bf-nav.js
+++
@@ -1,9 +0,0 @@
-export function initialize(container) {
-  var router = container.lookup('router:main');
-  container.register('service:router', router, { instantiate: false });
-}
-
-export default {
-  name: 'bf-nav',
-  initialize: initialize
-};
