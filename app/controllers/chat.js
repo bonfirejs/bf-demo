a8fc2ecd2345717f1e78b9ba0d969ee8259b1fc6
Nathan Ward <ward@digivine.com>
2015-03-31 14:41:01 -0700

    Add login, list of users, and nicer chat interface

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,18 +1,57 @@
 import Ember from 'ember';
+import md5 from 'npm:blueimp-md5/js/md5';
 
 export default Ember.Controller.extend({
-  actions: {
-    submitMessage: function() {
-      var chat = this.store.createRecord('chat', {
-        name: this.get('name'),
-        message: this.get('message')
-      });
-      chat.save().always(() => {
-        this.send('clearInput');
-      });
+  users: [
+    {
+      name: 'Nathan Ward',
+      email: 'ward@digivine.com',
+      image: 'http://www.gravatar.com/avatar/' + md5('ward@digivine.com') + '?s=60'
     },
-    clearInput: function() {
-      this.set('name', '');
+    {
+      name: 'Murali Sundar',
+      email: 'murali.sundar@intel.com',
+      image: 'http://www.gravatar.com/avatar/' + md5('murali.sundar@intel.com') + '?s=60'
+    }
+  ],
+  chats: [
+    {
+      user: {
+        name: 'Nathan Ward',
+        image: 'http://www.gravatar.com/avatar/' + md5('ward@digivine.com') + '?s=60',
+        isCurrentUser: true
+      },
+      message: 'Does this chat interface actually work?',
+      timestamp: Date.now()
+    },
+    {
+      user: {
+        name: 'Murali Sundar',
+        image: 'http://www.gravatar.com/avatar/' + md5('murali.sundar@intel.com') + '?s=60',
+        isCurrentUser: false
+      },
+      message: 'I guess so. Cool, dude.',
+      timestamp: Date.now()
+    }
+  ],
+  chatsLabel: function() {
+    return this.get('chats.length') + ' messages';
+  }.property('chats.length'),
+  usersLabel: function() {
+    return this.get('users.length') + ' users';
+  }.property('users.length'),
+  actions: {
+    sendMessage: function() {
+      var chat = {
+        user: {
+          name: this.get('name'),
+          image: 'http://www.gravatar.com/avatar/' + md5(this.get('email')) + '?s=60',
+          isCurrentUser: false
+        },
+        message: this.get('message'),
+        timestamp: Date.now()
+      };
+      this.get('chats').pushObject(chat);
       this.set('message', '');
     }
   }
