27b1a865774bcb295afbb8db1d77b63dd6f95bc3
Nathan Ward <ward@digivine.com>
2015-04-27 18:21:22 -0700

    Upgrade to ember-cli 0.2.3

--- app/router.js
+++ app/router.js
@@ -5,8 +5,6 @@ var Router = Ember.Router.extend({
   location: config.locationType
 });
 
-Router.map(function() {
+export default Router.map(function() {
   this.resource('chat', function() {});
 });
-
-export default Router;
